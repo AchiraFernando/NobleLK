41d52296c7b4a9041f635cd2d432a5b373daaa81
Achira Fernando <achirafernando97@gmail.com>
2021-04-08 20:43:31 +0530

    setting up firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyAZMWw8iTJJvqmHeE4wXOqjQcYLYbUg8EM",
+    authDomain: "noblelk.firebaseapp.com",
+    databaseURL: "https://noblelk-default-rtdb.firebaseio.com",
+    projectId: "noblelk",
+    storageBucket: "noblelk.appspot.com",
+    messagingSenderId: "1037894884221",
+    appId: "1:1037894884221:web:493ac87636fcbeec19ca14",
+    measurementId: "G-NQ5BVVVRJ7"
+  }
 };
